0cb85579969b2e32eae8f575ae60a2db8f003ee9
aleisabl <aleisabl.25@gmail.com>
2019-03-20 13:01:21 -0300

    se logro mostrar la data, falta que sirva con la condicion del input value..

--- main.js
+++ main.js
@@ -1,39 +1,52 @@
 const card = document.getElementById('card');
-let condition = document.getElementById('search-input').value;
+
 /* let url = 'http://www.omdbapi.com/?apikey=6faa8fa5&s=' + condition;
  */
 const getData = () => {
 
-    fetch('http://www.omdbapi.com/?apikey=6faa8fa5&s=Batman')
-        .then(resp => {
-            return resp.json();
-        })
+    let condition = document.getElementById('search').value;
 
-        .then(function (data) {
+    fetch('http://www.omdbapi.com/?apikey=6faa8fa5&t=The')
+        .then(resp => { return resp.json(); })
+
+        .then(data => {
             console.log(data)
 
             document.getElementById('search-btn').addEventListener('click', () => {
 
-                card.innerHTML =
+                document.getElementById('screen1').setAttribute('hidden', true);
+
+                card.innerHTML = "";
+                card.innerHTML +=
 
                 `   
                 <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="${JSON.stringify(data).Poster}" alt="poster">
+                <img class="card-img-top" src="${data.Poster}" alt="poster">
                 <div class="card-body">
+                <p class="card-text">${JSON.stringify(data.Genre)}|${JSON.stringify(data.Runtime)}|${JSON.stringify(data.Released)}</p>
                 <h5 class="card-title"></h5>
-                <p class="card-text">${JSON.stringify(data)}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <p class="card-text">${JSON.stringify(data.Title)}</p>
+                <a href="#" class="btn btn-primary">Ver más</a>
                 </div>
                 
                 `
 
             }) 
-
             
-        })
-
-        .catch(error => error)
+        });
 
 }
 
-window.onload = getData();
+/* let getData = () => {
+    fetch('http://www.omdbapi.com/?apikey=6faa8fa5&t=Batman').then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data)
+
+        card.innerHTML += JSON.stringify(data.Poster);
+
+    })
+}
+*/
+
+window.onload = getData(); 
